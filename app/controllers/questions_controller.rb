01e46799671a13a40350a6e3696e6d42eb3e54e6
uselesssas <sas.clicking@mail.ru>
2022-06-20 19:22:35 +0300

    Added the ability to create questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,33 @@ class QuestionsController < ApplicationController
     # Массив со всеми вопросами
     @questions = Question.all
   end
+
+  def show
+    # Ищет вопрос по id
+    @question = Question.find params[:id]
+  end
+
+  def new
+    # Инициализируем новый объект
+    @question = Question.new
+  end
+
+  # Создаёт вопрос в БД
+  def create
+    # render plain: params - Отображает отправленные данные
+    @question = Question.new question_params
+    if @question.save
+      flash[:success] = 'Question created!'
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Фильтрация params
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
