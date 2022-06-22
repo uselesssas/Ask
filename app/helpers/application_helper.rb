3494ee4e967d2e43b60d4152f000b34b79191f5d
uselesssas <sas.clicking@mail.ru>
2022-06-22 14:05:22 +0300

    Added helpers for title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = 'ASK'
+    # Если значение переменной page_title установлено
+    if page_title.present?
+      "#{base_title} — #{page_title}"
+    else
+      base_title
+    end
+  end
 end
