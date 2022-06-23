90a15f2cfc85b5e5dd3033b712f689a241aa247c
uselesssas <sas.clicking@mail.ru>
2022-06-23 15:47:54 +0300

    Adding a check at the DB level

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_23_102941) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_23_124455) do
   create_table "answers", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -20,8 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_23_102941) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
