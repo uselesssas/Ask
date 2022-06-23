014187c50fc18b1bf9643af06c51494faca5dec5
uselesssas <sas.clicking@mail.ru>
2022-06-23 18:12:49 +0300

    Checking whether the user is logged in

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -12,6 +12,20 @@ module Authentication
       current_user.present?
     end
 
+    def require_authentication
+      return if user_signed_in?
+
+      flash[:warning] = 'You are not signed in!'
+      redirect_to root_path
+    end
+
+    def require_no_authentication
+      return if !user_signed_in?
+
+      flash[:warning] = 'You are already signed in!'
+      redirect_to root_path
+    end
+
     def sign_in(user)
       session[:user_id] = user.id
     end
