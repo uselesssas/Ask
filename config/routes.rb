e1bd517571a35ccb14e2123d18db9c35b64d5b45
uselesssas <sas.clicking@mail.ru>
2022-06-23 14:32:47 +0300

    Added user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[new create]
+
   resources :questions, only: %i[index show new create edit update destroy] do
     resources :answers, expect: %i[index show new]
   end
